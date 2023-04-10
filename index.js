c307e8489eeabee609dd2f381d47137f3502f7bb
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-04-09 21:43:43 -0400

    completed array lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const appendNewCats = [...cats];
+    appendNewCats.push(name);
+    return appendNewCats;
+}
+
+function prependCat(name){
+    const prependNewCat = [...cats];
+    prependNewCat.unshift(name);
+    return prependNewCat;
+}
+
+function removeLastCat(){
+    const removeLastC = [...cats];
+    removeLastC.pop();
+    return removeLastC;
+}
+
+function removeFirstCat(){
+    const removeFirstC = [...cats];
+    removeFirstC.shift();
+    return removeFirstC;
+}
