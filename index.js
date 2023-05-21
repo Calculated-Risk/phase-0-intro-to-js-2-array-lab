7c587eb710e0d5d9b3025b20cfcf18f4397ec838
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-05-21 16:52:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendCat(name){
     cats.push(name);
@@ -9,34 +8,52 @@ function destructivelyPrependCat(name){
     cats.unshift(name);
 }
 
+//Array functions destructivelyRemoveLastCat()
+//removes the last cat from the cats array:
+
 function destructivelyRemoveLastCat(){
     cats.pop();
 }
+
+//Array functions destructivelyRemoveFirstCat()
+//removes the first cat from the cats array:
 
 function destructivelyRemoveFirstCat(){
     cats.shift();
 }
 
+//Array functions appendCat(name)
+//appends a cat to the cats array and returns a new array, leaving the cats array unchanged:
+
 function appendCat(name){
-    const appendNewCats = [...cats];
-    appendNewCats.push(name);
-    return appendNewCats;
+   const newCats = [...cats]
+   newCats.push(name)
+   return newCats
 }
+
+//Array functions prependCat(name)
+//prepends a cat to the cats array and returns a new array, leaving the cats array unchanged:
 
 function prependCat(name){
-    const prependNewCat = [...cats];
-    prependNewCat.unshift(name);
-    return prependNewCat;
+    const catsNew = [...cats];
+    catsNew.unshift(name);
+    return catsNew
 }
 
-function removeLastCat(){
-    const removeLastC = [...cats];
-    removeLastC.pop();
-    return removeLastC;
+//Array function  removeLastCat()
+// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+
+function removeLastCat(name){
+    const lastCat = [...cats];
+    lastCat.pop()
+    return lastCat
 }
 
-function removeFirstCat(){
-    const removeFirstC = [...cats];
-    removeFirstC.shift();
-    return removeFirstC;
+//Array functions removeFirstCat()
+// removes the first cat from the cats array and returns a new array, leaving the cats array unchanged:
+
+function removeFirstCat(name){
+    const firstCat = [...cats];
+ firstCat.shift()
+ return firstCat;
 }
